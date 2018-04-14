737f0855067e1da8c0af12e24b82a15f05739f9a
Miłosław Politowski <miloslaw.politowski@outlook.com>
2018-04-14 14:23:42 +0200

    improvements for heroku deployment

--- scripts/server.js
+++ scripts/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const path = require('path');
 
 const { AQICN_TOKEN } = process.env;
 const AQICN_URL_SEARCH = 'https://api.waqi.info/search/?keyword=';
 const AQICN_URL_FEED = 'https://api.waqi.info/feed/';
 
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 3002;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../build')));
+}
 
 app.get('/api/search/:keyword', async (req, res) => {
     let { keyword } = req.params;
