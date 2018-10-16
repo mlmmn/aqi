d541658c710c49e4b6db4f8ac913f71734c44646
Miłosław Politowski <miloslaw.politowski@outlook.com>
2018-10-16 18:38:44 +0200

    Remove demo from heroku

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Air Quality Index
-
-[View demo](https://ancient-eyrie-72742.herokuapp.com)
--- index.js
+++ index.js
@@ -13,106 +13,96 @@ const app = express();
 const port = process.env.PORT || 3002;
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 }
 
 function checkAqi(aqi) {
-    return !!parseInt(aqi, 10);
+  return !!parseInt(aqi, 10);
 }
 
 function getRatingData(aqi) {
-    let returnObj;
+  let returnObj;
 
-    for (let rating in aqiRating) {
-
-        if (aqi >= parseInt(rating, 10)) {
-            returnObj = {
-                rating: aqiRating[rating]["rating"],
-                description: aqiRating[rating]["description"],
-                color: aqiRating[rating]["tag"]
-            }
-        }
+  for (let rating in aqiRating) {
+    if (aqi >= parseInt(rating, 10)) {
+      returnObj = {
+        rating: aqiRating[rating]['rating'],
+        description: aqiRating[rating]['description'],
+        color: aqiRating[rating]['tag']
+      };
     }
+  }
 
-    return returnObj;
+  return returnObj;
 }
 
 app.get('/api/search/:keyword', async (req, res) => {
-    let { keyword } = req.params;
-    let response;
-    let data;
+  let { keyword } = req.params;
+  let response;
+  let data;
 
-    try {
-        response = await fetch(`${AQICN_URL_SEARCH}${encodeURI(keyword)}&token=${AQICN_TOKEN}`);
+  try {
+    response = await fetch(`${AQICN_URL_SEARCH}${encodeURI(keyword)}&token=${AQICN_TOKEN}`);
 
-        if (response.ok) {
-            data = await response.json();
-            data = await data.data;
+    if (response.ok) {
+      data = await response.json();
+      data = await data.data;
 
-            data = data.filter((item) => {
-                return checkAqi(item.aqi);
-            });
+      data = data.filter(item => {
+        return checkAqi(item.aqi);
+      });
 
-            data = data.map((item) => {
-               return Object.assign(
-                   item,
-                   {
-                       color: getRatingData(item.aqi).color
-                   }
-               )
-            });
-        } else {
-            throw new Error('Data could not be fetched from server');
-        }
-
-        res.send(data);
+      data = data.map(item => {
+        return Object.assign(item, {
+          color: getRatingData(item.aqi).color
+        });
+      });
+    } else {
+      throw new Error('Data could not be fetched from server');
     }
-    catch (error) {
-        res.status(500).json({
-            error: error.toString()
-        })
-    }
+
+    res.send(data);
+  } catch (error) {
+    res.status(500).json({
+      error: error.toString()
+    });
+  }
 });
 
 app.get('/api/getstation/:id', async (req, res) => {
-    let { id } = req.params;
-    let response;
-    let data;
+  let { id } = req.params;
+  let response;
+  let data;
 
-    try {
-        for (let i = 0; i < 3; i++) {
-            response = await fetch(`${AQICN_URL_FEED}@${encodeURI(id)}/?token=${ AQICN_TOKEN }`);
-            data = await response.json();
-            data = data.data;
+  try {
+    for (let i = 0; i < 3; i++) {
+      response = await fetch(`${AQICN_URL_FEED}@${encodeURI(id)}/?token=${AQICN_TOKEN}`);
+      data = await response.json();
+      data = data.data;
 
-            if (!!data) {
-                break;
-            }
-        }
-
-        data = Object.assign(
-            data,
-            getRatingData(data.aqi)
-        );
-
-        for (let prop in data.iaqi) {
-
-            if (measurementParams.hasOwnProperty(prop)) {
-                data.iaqi[prop].name = measurementParams[prop];
-            } else {
-                delete data.iaqi[prop];
-            }
-        }
-
-        res.send(data);
+      if (!!data) {
+        break;
+      }
     }
-    catch (error) {
-        res.status(500).json({
-            error: error.toString()
-        })
+
+    data = Object.assign(data, getRatingData(data.aqi));
+
+    for (let prop in data.iaqi) {
+      if (measurementParams.hasOwnProperty(prop)) {
+        data.iaqi[prop].name = measurementParams[prop];
+      } else {
+        delete data.iaqi[prop];
+      }
     }
+
+    res.send(data);
+  } catch (error) {
+    res.status(500).json({
+      error: error.toString()
+    });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
